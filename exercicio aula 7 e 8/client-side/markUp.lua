e31c6cc6bdf3125222761cee647ea3afb156f00b
Rafael Moura <rafaelcostamoura@gmail.com>
2022-11-06 18:21:06 -0300

    fix Error

--- exercicio aula 7 e 8/client-side/markUp.lua
+++ exercicio aula 7 e 8/client-side/markUp.lua
@@ -6,11 +6,11 @@ local listPosicoes = {
     [1] = { ['x'] = 132.91, ['y'] = -989.7, ['z'] = 29.36 },
     [2] = { ['x'] = 169.48, ['y'] = -1003.86, ['z'] = 29.35},
     [3] = { ['x'] = 211.19, ['y'] = -1017.45, ['z'] = 29.31},
-    [4] = { ['x'] = 23.96, ['y'] = -980.54, ['z'] = 29.3 },
-    [6] = { ['x'] = 239.27, ['y'] = -939.34, ['z'] = 29.29},
-    [7] = { ['x'] = 263.69, ['y'] = -872.88, ['z'] = 29.17 },
-    [8] = { ['x'] = 224.49, ['y'] = -858.74, ['z'] = 30.12 },
-    [9] = { ['x'] = 185.85, ['y'] = -844.58, ['z'] = 31.1 }
+    [4] = { ['x'] = 224.96, ['y'] = -980.54, ['z'] = 29.3 },
+    [5] = { ['x'] = 239.27, ['y'] = -939.34, ['z'] = 29.29},
+    [6] = { ['x'] = 263.69, ['y'] = -872.88, ['z'] = 29.17 },
+    [7] = { ['x'] = 224.49, ['y'] = -858.74, ['z'] = 30.12 },
+    [8] = { ['x'] = 185.85, ['y'] = -844.58, ['z'] = 31.1 }
 }
 
 local pos = 1
@@ -21,17 +21,18 @@ Citizen.CreateThread(function()
         local pedPalyer = PlayerPedId()
         local coordsPlayer = GetEntityCoords(pedPalyer)
             local diffDistance = #(vector3(coordsPlayer[1], coordsPlayer[2], coordsPlayer[3]) - vector3(listPosicoes[pos].x,listPosicoes[pos].y,listPosicoes[pos].z))
-            if diffDistance < 10.0 then 
-                time = 1
-                DrawMarker(21,listPosicoes[pos].x,listPosicoes[pos].y,listPosicoes[pos].z,0,0,0,0,0,0,1.00,1.00,1.00,255,6,6,100,0,0,0,1)
+            if diffDistance < 80.0 then 
+                timeWaiting = 5
+                DrawMarker(21,listPosicoes[pos].x,listPosicoes[pos].y,listPosicoes[pos].z,0,0,0,0,0,0,2.00,2.00,2.00,255,6,6,100,0,0,0,1)
 
                 if diffDistance < 2.0 then 
-                    drawTxt("PRESSIONE  ~g~E~w~  PARA SELECIONAR", 4, 0.5, 0.93, 0.50, 255, 255, 255,180)
+                    drawTxt("PRESSIONE  ~g~E~w~  PARA SELECIONAR", 4, 0.5, 0.80, 0.50, 255, 255, 255,180)
+                    SetNuiFocus(false,false)
                     if IsControlJustPressed(0,38) then 
                         if pos == #listPosicoes then 
                             pos = 1
                         else 
-                            pos = pos + 1
+                            pos = pos + 1 
                             TriggerEvent("sounds:source","cash",1)
                         end
                     end
